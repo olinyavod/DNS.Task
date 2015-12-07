c245cc7fa611fc963d1334d9714300420cbff118
olinyavod <olinyavod@gmail.com>
2015-12-07 23:53:46 +1000

    Change type for primary key

---
+++ DNS.Task/DNS.Task.Core/Store/StoredProcedureNodesStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DNS.Task.Core.Models;
+
+namespace DNS.Task.Core.Store
+{
+	public class StoredProcedureNodesStore: AdoStoreBase, ICrudStore<Node, int>
+	{
+		public StoredProcedureNodesStore(AdoUnitOfWork unit) : base(unit)
+		{
+		}
+
+		public int Add(Node entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Update(Node entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Node Get(int key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Node Get<TFilter>(TFilter filter) where TFilter : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Node> GetList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Node> GetList<TFilter>(TFilter filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ForceDelete(int key)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
