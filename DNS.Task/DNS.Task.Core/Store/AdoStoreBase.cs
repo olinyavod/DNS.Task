8521fc3631f661a345d908e0f17b0c2be1f0ba1b
olinyavod <olinyavod@gmail.com>
2015-12-07 23:20:37 +1000

    Create base store for ADO.NET

---
+++ DNS.Task/DNS.Task.Core/Store/AdoStoreBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace DNS.Task.Core.Store
+{
+	public class AdoStoreBase
+	{
+		private readonly AdoUnitOfWork _unit;
+
+		public AdoStoreBase(AdoUnitOfWork unit)
+		{
+			_unit = unit;
+		}
+
+		protected SqlCommand CreateSqlCommand(string sql, params SqlParameter[] parameters)
+		{
+			return _unit.CreateSqlCommand(sql, parameters);
+		}
+
+		protected SqlCommand CreateStoreCommand(string store, params SqlParameter[] parameters)
+		{
+			return _unit.CreateStoreCommand(store, parameters);
+		}
+
+		protected void Rollback()
+		{
+			_unit.Rollback();
+		}
+	}
+}
